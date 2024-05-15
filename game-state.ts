b6cf6ea8ce4c55fd720e22f2cd0fee65d72f33f3
oscisn93 <ocisneros1@csu.fullerton.edu>
2024-05-15 12:54:45 -0700

    added firebase anonymous auth

--- game-state.ts
+++
@@ -1,7 +0,0 @@
-import type { Point } from "./types.d.ts";
-
-export class GameState {
-  private players: Record<string, string> = {};
-  private eggs: Record<string, Point> = {};
-  constructor(private gameID: string) {}
-}
